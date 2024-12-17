3095d682ed1fde1a5ffde7342e253648012faee9
Jestoni <jhontobit16@gmail.com>
2024-12-18 02:32:51 +0800

    first commit12

--- src/app/client/[clientUrl]/page.tsx
+++ src/app/client/[clientUrl]/page.tsx
@@ -1,39 +1,17 @@
 'use client';
 
 import styles from './page.module.css';
+import Link from 'next/link';  
 import Image from 'next/image';
-import Link from 'next/link';
-
 const RegisterForm: React.FC = () => {
 
  
   return (
     <div className={styles.page1}>
-      <div className={styles.leftSide}>
-        {/* Optimized Image Component */}
-        <Image src="/jesua.png" alt="Jesua Garlet" width={500} height={500} priority />
-        <Link href="/asas" className={styles.leftLink}>
-          My Listings
-        </Link>
+      <div className={styles.imgContainer}><Image src="/jesua.webp" width={2560} height={1440} alt="" />
+      <Link href={'asas'} className={styles.linkList}> My Listings</Link>
       </div>
 
-      <div className={styles.rightSide}>
-        <div className={styles.name}>
-          <h1>Jesua Garlet</h1>
-          <p>Real Estate Specialist</p>
-        </div>
-
-        {/* Dynamic Content */}
-        <ul>
-          <li>PRC LICENSED AND DSHUD REGISTERED</li>
-          <li>Number: 09662996401</li>
-          <li>Email: jesuagarletbesthomes@gmail.com</li>
-          <li>Website: BeuatifulHomesPH.com</li>
-          <li>Address: Cebu City Cebu</li>
-          {/* Display the dynamic ClientUrl parameter */}
-
-        </ul>
-      </div>
     </div>
   );
 }
