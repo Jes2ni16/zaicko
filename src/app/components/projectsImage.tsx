cae78b75676bae2653886b74be7a2557ba8b433a
Jes2ni16 <jhontobit16@gmail.com>
2025-01-12 17:32:58 +0800

    add list table

---
+++ src/app/components/projectsImage.tsx
@@ -0,0 +1,151 @@
+import { useState } from 'react';
+import { Dialog, DialogContent, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Image from 'next/image';
+
+interface Image {
+  src: string;
+  alt: string;
+}
+
+interface ImageGalleryProps {
+  images: Image[];
+}
+
+const ImageGallery = ({ images }: ImageGalleryProps) => {
+  const [open, setOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<Image | null>(null);
+  const [currentIndex, setCurrentIndex] = useState<number | null>(null);
+
+  const handleOpen = (image: Image, index: number) => {
+    setSelectedImage(image);
+    setCurrentIndex(index);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedImage(null);
+    setCurrentIndex(null);
+  };
+
+  const handleNext = () => {
+    if (currentIndex !== null) {
+      const nextIndex = (currentIndex + 1) % images.length;
+      setSelectedImage(images[nextIndex]);
+      setCurrentIndex(nextIndex);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentIndex !== null) {
+      const prevIndex = (currentIndex - 1 + images.length) % images.length;
+      setSelectedImage(images[prevIndex]);
+      setCurrentIndex(prevIndex);
+    }
+  };
+
+  return (
+    <>
+      {/* Image Container */}
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '30px' }}>
+        {images.map((image, index) => (
+          <div
+            key={index}
+            onClick={() => handleOpen(image, index)}
+            style={{ cursor: 'pointer', width: '300px', height: '300px' }}
+          >
+            <Image
+              src={image.src}
+              alt={image.alt}
+              width={300}
+              height={300}
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+        ))}
+      </div>
+
+      {/* Full-Screen Dialog */}
+      {selectedImage && currentIndex !== null && (
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          fullScreen
+          PaperProps={{
+            style: { backgroundColor: 'rgba(0, 0, 0, 0.9)', boxShadow: 'none' },
+          }}
+        >
+          <DialogContent style={{   position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', }}>
+            {/* Close Button */}
+            <IconButton
+              onClick={handleClose}
+              style={{
+                position: 'absolute',
+                top: '16px', // Adjust the distance from the top
+                right: '16px', // Adjust the distance from the right
+                zIndex: 1, // Ensure it's on top of other elements
+                backgroundColor: 'rgba(0, 0, 0, 0.5)', // Optional: semi-transparent background
+                borderRadius: '50%', // Make the button round
+                padding: '8px', // Adjust padding for better clickable area
+              }}
+            >
+              <CloseIcon style={{ color: 'white' }}/>
+            </IconButton>
+
+            {/* Next Button */}
+            <IconButton
+              onClick={handlePrevious}
+              style={{
+                position: 'absolute',
+                top: '50%',
+                left: 10,
+                transform: 'translateY(-50%)',
+                color: 'white',
+              }}
+            >
+              <ArrowBackIcon />
+            </IconButton>
+
+            {/* Full-Screen Image */}
+            <Image
+              src={selectedImage.src}
+              alt={selectedImage.alt}
+              width={600} // Larger width for fullscreen view
+              height={600} // Larger height for fullscreen view
+              style={{
+                width: '100%', 
+                maxWidth: '900px',
+                maxHeight:'900px',
+                height: 'auto',
+                objectFit: 'contain',
+              }}
+            />
+
+            {/* Previous Button */}
+            <IconButton
+              onClick={handleNext}
+              style={{
+                position: 'absolute',
+                top: '50%',
+                right: 10,
+                transform: 'translateY(-50%)',
+                color: 'white',
+              }}
+            >
+              <ArrowForwardIcon />
+            </IconButton>
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
+  );
+};
+
+export default ImageGallery;
--- src/app/projects/mandtra/page.tsx
+++ src/app/projects/mandtra/page.tsx
@@ -1,10 +1,58 @@
 'use client';
 
 import styles from './page.module.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'next/image';
+import ProjectsImage from '..//../components/projectsImage';
 
 export default function Mandtra() {
+
+  const mandtra = [
+     { src: 'https://i.imgur.com/6jjNq8v.jpg', alt: 'Image 1' },
+    { src: 'https://i.imgur.com/xqXLrSI.jpg', alt: 'Image 2' },
+    { src: 'https://i.imgur.com/1sDAOHS.png', alt: 'Image 3' },
+    ]
+
+    const oneBedroom = [
+      { src: 'https://i.imgur.com/IwAzT7X.jpg', alt: 'Image 1' },
+      { src: 'https://i.imgur.com/G360d6w.jpg', alt: 'Image 2' },
+    ]
+
+    const studioUnit = [
+      { src: 'https://i.imgur.com/TAuuzI0.jpg', alt: 'Image 1' },
+      { src: 'https://i.imgur.com/hy4UVQg.jpg', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/9ektkrW.jpg', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/7IYUAXT.jpg', alt: 'Image 2' },
+    ]
+
+    const tower1 = [
+      { src: 'https://i.imgur.com/MYprD1c.jpg', alt: 'Image 1' },
+      { src: 'https://i.imgur.com/cs9AyRX.jpg', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/AruhKta.jpg', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/HlbychQ.jpg', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/yMNUJO6.jpg', alt: 'Image 2' },
+    ]
+    const tower2 = [
+      { src: 'https://i.imgur.com/qCh9Gz7.jpg', alt: 'Image 1' },
+      { src: 'https://i.imgur.com/aFTkeDr.jpg', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/2R9xrO5.jpg', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/LC0g6eE.jpg', alt: 'Image 2' },
+    ]
+    const tower3 = [
+      { src: 'https://i.imgur.com/Gm20Rpz.jpg', alt: 'Image 1' },
+      { src: 'https://i.imgur.com/aVLlZ8S.jpg', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/fqas53G.jpg', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/2h2yZX0.jpg', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/NJ9CwUj.jpg', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/QokPemi.jpg', alt: 'Image 2' },
+    ]
+    const amenities = [
+      { src: 'https://i.imgur.com/OpTcABy.png', alt: 'Image 1' },
+      { src: 'https://i.imgur.com/zBkEiQk.png', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/XEaDYen.png', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/WvmrSjU.png', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/VhLb1GW.png', alt: 'Image 2' },
+      { src: 'https://i.imgur.com/9iNLwp5.png', alt: 'Image 2' },
+    ]
 
 
   return (
@@ -31,10 +79,8 @@ export default function Mandtra() {
 <p>
 MANDTRA RESIDENCES is an urban tropical living built for the well balanced Cebuano life. Rooted living because of Cebuano traditions, Cebuano craftmanship and artistry. Modern and well balanced living as it brings warmth of a home in the midst of a busy city. Located at P. Basubas St. Tipolo, Mandaue Cebu City. A joint venture under Cebu Homegrown Developers, Inc. Owned and Managed by Cebu Landmasters, Inc. In partnership with Ixidor Holdings Inc.
 </p>
-<div className="d-flex align-items-center flex-wrap gap-5">
-<Image src='https://i.imgur.com/6jjNq8v.jpg' width='300' height={300} alt='MANDTRA RESIDENCES'/>
-<Image src='https://i.imgur.com/xqXLrSI.jpg'  width='300' height={300} alt='MANDTRA RESIDENCES'/>
-<Image src='https://i.imgur.com/1sDAOHS.png'  width='300' height={300} alt='MANDTRA RESIDENCES'/>
+<div className="">
+<ProjectsImage images={mandtra} />
 </div>
 
 </div>
@@ -46,6 +92,121 @@ MANDTRA RESIDENCES is an urban tropical living built for the well balanced Cebua
 </div>
 </div>
 
+<div className={styles.fourthFold}>
+<h3>Floor Plans</h3>
+<div className={styles.imgContainer}>
+  <h4 style={{marginBottom:'10px'}}>1 Bedroom</h4>
+<div className=''>
+<ProjectsImage images={oneBedroom} />
+</div>
+</div>
+<div className={styles.imgContainer}>
+  <h4 style={{marginBottom:'10px'}}>Studio Unit</h4>
+<div className=''>
+<ProjectsImage images={studioUnit} />
+</div>
+</div>
+<div className={styles.imgContainer}>
+  <h4>Tower 1</h4>
+<div className={styles.tower1}>
+<ProjectsImage images={tower1} />
+</div>
+</div>
+<div className={styles.imgContainer}>
+  <h4>Tower 2</h4>
+<div className={styles.tower2}>
+<ProjectsImage images={tower2} />
+</div>
+</div>
+<div className={styles.imgContainer}>
+  <h4>Tower 3</h4>
+<div className={styles.tower3}>
+<ProjectsImage images={tower3} />
+</div>
+</div>
+
+</div>
+
+<div className={styles.fifthFold}>
+<h2>PROJECT DETAILS</h2>
+<p style={{margin:'20px 0', fontSize:'20px'}}>
+Residential Condominium with Retail Areas.
+</p>
+<div className={styles.details}>
+<p>1.24 Hecatres</p>
+<p>3 Residential Towers</p>
+<p>3 Levels of Podium Parking (GR, 2nd, 3rd Floor)</p>
+<p>Retail Component</p>
+<p>PHASE 1: Residential Tower 1</p>
+<ul>
+  <li>26th Floor</li>
+  <li>595 Units</li>
+</ul>
+<p>PHASE 2: Residentail TOwer 2 - TBA</p>
+<p>PHASE 3 : Residential Tower 3 - TBA</p>
+<p>BUILDING FEATURES</p>
+<ul>
+  <li>Lobby with Reception Area</li>
+  <li>3 Passenger Elevators</li>
+  <li>Mail Room</li>
+  <li>24/7Security</li>
+  <li>Combinable Units</li>
+  <li>Garbage Holding Room per floor</li>
+  <li>Administration Office</li>
+  <li>Property Management Services</li>
+  <li>Property Management Services</li>
+</ul>
+<p>UNIT FEATURES</p>
+<ul>
+  <li>Painted Wall, Partitions and Ceilings</li>
+  <li>Vinyl Planks Flooring</li>
+  <li>
+  Ceramic Non-slip floor tiles for T & B and Balcony
+  </li>
+  <li>Complete Bathroom Fixtures with Provision for Shower Heater Outlet</li>
+<li>Provision for Kitchen Base Cabinet with Granite Counter Top</li>
+<li>Provision for Smoke Detector and Suppression System</li>
+<li>Provision for Air-conditioning Opening and Outlet</li>
+<li>Provision for Cable TV, Internet & Telephone Connection</li>
+</ul>
+<p>UNIT TYPES</p>
+<ul>
+  <li>Studio Unit</li>
+  <li>1 Bedroom Unit</li>
+</ul>
+</div>
+
+</div>
+
+<div className={styles.amenities}>
+<h2>Amenities</h2>
+<p style={{fontSize:'20px'}}>Community Amenities</p>
+<ul>
+  <li>Sky Garden</li>
+  <li>Retail Areas</li>
+  <li>Adult & Kiddie Pool</li>
+  <li>Jogging Paths</li>
+  <li>Clubhouse</li>
+  <li>Children&apos;s Playground</li>
+<li>Shower Area</li>
+<li>Function Hall</li>
+<li>Chapel</li>
+<li>Landscape Areas</li>
+<li>Wellness Lounge</li>
+</ul>
+<div className={styles.imgs}>
+<ProjectsImage images={amenities} />
+</div>
+</div>
+
+
+<div className={styles.constUpdate}>
+<h2>Construction Updates</h2>
+
+<div className={styles.imgs}>
+<ProjectsImage images={amenities} />
+</div>
+</div>
 
     </div>
   );
