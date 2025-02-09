f7cecdbfbbaadd1a9efb1f194a9abc92337982a0
Jes2ni16 <jhontobit16@gmail.com>
2025-02-09 23:35:37 +0800

    add projects

---
+++ src/app/client/[clientUrl]/featured/casa-mira-mandaue/page.tsx
@@ -0,0 +1,207 @@
+
+import { Metadata } from 'next'
+import FullImage from '../fullScreenImg';
+import styles from '../page.module.css';
+import Image from 'next/image';
+
+  
+
+
+  export const metadata: Metadata = {
+    title: 'CASA MIRA TOWERS MANDAUE ',
+    description: 'Casa Mira Towers Mandaue is a 4 high-rise residential condominium development that sits in one of Cebu major cities. The development offers resort like amenities, thus will offer MORE to the economic market.',
+    openGraph: {
+      title: 'CASA MIRA TOWERS MANDAUE  | Luxury Town House',
+      description: 'Casa Mira Towers Mandaue is a 4 high-rise residential condominium development that sits in one of Cebu major cities. The development offers resort like amenities, thus will offer MORE to the economic market.',
+      images: ['https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107947/A1-768x573_pqtqqi.webp'],
+    },
+  }
+
+  export default async function CasaMiraMandaue() {
+    return (
+    <div className={styles.body1}>
+
+<Image src='https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107947/A1-768x573_pqtqqi.webp' width={500} height={500} className={styles.heroImage} alt='' sizes="(max-width: 1540px) 100vw, 1540px"/>
+
+<section className={styles.section}>
+<h1>CASA MIRA TOWERS MANDAUE</h1>
+<p><strong>Casa Mira Towers Mandaue</strong>&nbsp;offers up to 60% open space that allows every resident to experience a more relaxing living experience and enjoy its surroundings.</p>
+<p><strong>Casa Mira Towers Mandaue</strong>&nbsp;has a total of 1,484 units.</p>
+<p>Tower 1: 12 Floors &ndash; 256 units<br />Tower 2: 19 Floors &ndash; 407 units<br />Tower 3: 23 Floors &ndash; 505 units<br />Tower 4: 15 Floors &ndash; 316 units</p>
+<p><em>Note: All units are combinable</em></p>
+<p><strong>Casa Mira Towers Mandaue</strong>&nbsp;is a 4 high-rise residential condominium development that sits in one of Cebu&rsquo;s major cities. The development offers resort like amenities, thus will offer MORE to the economic market.</p>
+<p>Get a gaze at the bustling city of Mandaue in a development that fuses the conveniences of business, leisure &amp; entertainment.</p>
+<p><strong>Casa Mira Towers Mandaue</strong>&nbsp;is surrounded by major establishments like:</p>
+<ul>
+<li>St. Louis School</li>
+<li>St. Joseph National Shrine</li>
+<li>Pacific Mall</li>
+<li>Prince Warehouse</li>
+<li>Dr. Ignacio M. Cortes General Hospital</li>
+<li>Citi Hardware</li>
+</ul>
+<p>This newest address quickly links you to either the north or south part of Cebu, and even to Mactan Island.</p>
+
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107947/A2-768x441_fe9krb.webp" alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px"/>
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107946/A3-768x575_mvanwk.webp"  alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px" />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107947/A4-768x577_icxfta.webp"  alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px" />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107945/A5-768x578_d38rfk.webp" alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px" />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107946/A6-768x435_y6ibow.webp"  alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px" />
+</section>
+<hr />
+<section className={styles.section}>
+<h2>LOCATION AND ACCESSIBILITY:</h2>
+<ul>
+<li>15 minutes to Mactan Cebu International Airport</li>
+<li>8 minutes to St. Joseph National Shrine &amp; St. Joseph Academy</li>
+<li>8 minutes to Mandaue City Hall</li>
+<li>8 minutes to Seamen&rsquo;s Hospital</li>
+<li>5 minutes to Pacific Mall</li>
+</ul>
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107946/A7-768x578_iuhmt8.webp"   alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px"  />
+</section>
+<hr />
+
+<section className={styles.section}>
+<h2>BUILDING FEATURES AND AMENITIES:</h2>
+<ul>
+<li>Adult and Kiddie Pool</li>
+<li>Fitness gym</li>
+<li>Clubhouse</li>
+<li>Play Area</li>
+<li>Multi-function Halls</li>
+<li>3 Elevators per Building</li>
+<li>Garbage rooms per floor</li>
+<li>100% back-up power</li>
+<li>24/7 CCTV system</li>
+<li>Retail Strip</li>
+<li>Property Management Services</li>
+</ul>
+<div className={styles.flexImage}>
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107961/B1-1024x766_gfbjss.webp"  alt="" />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107961/B2-1024x769_jwwqvl.webp"  alt=""  />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107962/B3-1024x766_sxm6sh.webp"  alt="" />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107963/B4-1024x769_cs2z87.webp"  alt=""  />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107952/B5-1024x763_ibcszy.webp"  alt=""  />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107953/B6-1024x766_pnok92.webp"  alt="" />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107953/B7-1024x763_td9i8y.webp"  alt=""  />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107954/B8-1024x765_whbw6o.webp"  alt="" />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107954/B9-1024x766_syzk3q.webp"  alt="" />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107955/B10-1024x768_w7jd92.webp"  alt="" />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107956/B11_o8c5gc.webp"   alt=""  />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107957/B12-1024x763_y031kh.webp"  alt=""  />
+</div>
+</section>
+<hr />
+
+<section className={styles.section}>
+<h2>UNIT TYPES and SIZES:</h2>
+<ul>
+<li>Typical Studio &ndash; 20.13 sqm</li>
+<li>Studio with Balcony &ndash; 23.31 sqm</li>
+<li>Special studio w/o Balcony &ndash; 24.44 sqm</li>
+<li>1-Bedroom w/ Balcony &ndash; 35.67 sqm</li>
+</ul>
+<div className={styles.flexImage}>
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107957/U1-1024x766_rwiefe.webp"  alt="" />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107958/U2-1024x771_cqxbg2.webp"  alt=""  />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107959/U3-1024x770_daxojl.webp"  alt=""  />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107958/U2-1024x771_cqxbg2.webp"  alt=""  />
+</div>
+<h2><strong>UNIT FEATURES (Deliverable Finishes)</strong></h2>
+<ul>
+<li>Tiled Floors</li>
+<li>Painted Walls</li>
+<li>T&amp;B Ceramic non-slip floor Tiles with Mechanical Exhaust</li>
+<li>Bathroom Fixtures</li>
+<li>Kitchen Base cabinet with counter-top</li>
+<li>Provision for Kitchen Exhaust</li>
+<li>Provision for Cable TV, Telephone/Internet</li>
+<li>Fire detection and suppression system</li>
+<li>Aircon Provision</li>
+<li>Provision for Shower Heater</li>
+</ul>
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107977/unit-features-768x574_ekuhew.webp"  alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px" />
+</section>
+<hr />
+
+<section className={styles.section}>
+<h2>SAMPLE COMPUTATIONS:</h2>
+<h3>TOWER 1 STUDIO UNIT:</h3>
+<h3>FLOOR AREA: 20.12 sq.m.</h3>
+<h3>FLOOR LEVEL: 6TH FLOOR</h3>
+<h3>BANK FINANCING:</h3>
+<p>LIST PRICE:&nbsp;<strong>Php 2,898,070.00</strong></p>
+<p>TRANSFER CHARGE<em>: Php 231,845.60</em></p>
+<p>RESERVATION FEE:<em>&nbsp;Php 15,000.00</em></p>
+<p>TOTAL CONTRACT PRICE:&nbsp;<strong>Php 3,129,915.60</strong></p>
+<p>&spades; 10% DP payable in&nbsp;60&nbsp;months:</p>
+<ul>
+<li><strong>Php 5,000.00/month</strong></li>
+</ul>
+<p>&spades; 90% BALANCE THROUGH BANK FINANCING:</p>
+<ul>
+<li><strong>Php 2,814,915.60</strong></li>
+</ul>
+
+<h3>TOWER 2 STUDIO UNIT:</h3>
+<h4>FLOOR AREA: 20.13 sq.m.</h4>
+<h4>FLOOR LEVEL: 10TH FLOOR</h4>
+<h4>BANK FINANCING:</h4>
+<p>LIST PRICE:&nbsp;<strong>Php 2,783,311.30</strong></p>
+<p>TRANSFER CHARGE<em>: Php 222,664.90</em></p>
+<p>RESERVATION FEE:<em>&nbsp;Php 8,000.00</em></p>
+<p>TOTAL CONTRACT PRICE:&nbsp;<strong>Php 3,005,976.21</strong></p>
+<p>&spades; 10% DP payable in&nbsp;<u>48</u>&nbsp;months:</p>
+<p><strong>Php 6,100.00/month</strong></p>
+<p>&spades; 90% BALANCE THROUGH BANK FINANCING:</p>
+<p><strong>Php 2,705,176.21</strong></p>
+
+<h3>TOWER 3 STUDIO UNIT:</h3>
+<h4>FLOOR AREA: 20.13 sq.m.</h4>
+<h4>FLOOR LEVEL: 10TH FLOOR</h4>
+<h4>BANK FINANCING:</h4>
+<p>LIST PRICE:&nbsp;<strong>Php 2,650,772.67</strong></p>
+<p>TRANSFER CHARGE<em>: Php 212,061.81</em></p>
+<p>RESERVATION FEE:<em>&nbsp;Php 8,000.00</em></p>
+<p>TOTAL CONTRACT PRICE:&nbsp;<strong>Php 2,862,834.48</strong></p>
+<p>&spades; 10% DP payable in&nbsp;<u>42</u>&nbsp;months:</p>
+<p>Php 6,700.00/month</p>
+<p>&spades; 90% BALANCE THROUGH BANK FINANCING:</p>
+<p>Php 2,573,434.48</p>
+
+<h3>TOWER 4 STUDIO UNIT:</h3>
+<h4>FLOOR AREA: 20.13 sq.m.</h4>
+<h4>FLOOR LEVEL: 10TH FLOOR</h4>
+<h4>BANK FINANCING:</h4>
+<p>LIST PRICE:&nbsp;<strong>Php 2,650,772.67</strong></p>
+<p>TRANSFER CHARGE<em>: Php 212,061.81</em></p>
+<p>RESERVATION FEE:<em>&nbsp;Php 8,000.00</em></p>
+<p>TOTAL CONTRACT PRICE:&nbsp;<strong>Php 2,862,834.48</strong></p>
+<p>&spades; 10% DP payable in&nbsp;<u>42</u>&nbsp;months:</p>
+<p>Php 6,700.00/month</p>
+<p>&spades; 90% BALANCE THROUGH BANK FINANCING:</p>
+<p>Php 2,573,434.48</p>
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107976/FP4_zu0wf1.webp" alt=""   className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px"/>
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107979/UP2-768x578_iuhil7.webp"  alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px"/>
+</section>
+<hr />
+
+<section className={styles.section}>
+<h2>CONSTRUCTION UPDATE!!!</h2>
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107980/UP3-1024x574_gske27.webp" alt=""  className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px" />
+</section>
+<hr />
+<section className={styles.section}>
+<h2>Floor Plans</h2>
+<div className={styles.flexImage}>
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107979/FP1_zuv4jz.webp"  alt=""  />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107981/FP2_zc93zs.webp" alt=""  />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107982/FP3_bia7vt.webp"  alt=""  />
+<FullImage src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739107978/UP1-768x574_a0h5w9.webp"  alt="" />
+</div>
+</section>
+
+    </div>
+)
+}
---
+++ src/app/client/[clientUrl]/featured/casa-mira-south/page.tsx
@@ -0,0 +1,296 @@
+
+import { Metadata } from 'next'
+import FullImage from '../fullScreenImg';
+import styles from '../page.module.css';
+import Image from 'next/image';
+
+
+export const metadata: Metadata = {
+    title: 'CASA MIRA SOUTH NAGA CITY, CEBU ',
+    description: 'Awarded as 2018 s Best Housing Development in Cebu, this beautifully designed economic community sits on a rolling terrain 60 to 100 meters above sea level that boasts expansive views and 45% open spaces.',
+    openGraph: {
+      title: 'CASA MIRA SOUTH NAGA CITY, CEBU  | Luxury Town House',
+      description: 'Awarded as 2018 s Best Housing Development in Cebu, this beautifully designed economic community sits on a rolling terrain 60 to 100 meters above sea level that boasts expansive views and 45% open spaces.',
+      images: ['https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104319/photo-townhouse-model-b-1_o4p0av.webp'],
+    },
+  }
+  
+  export default function CityscapeGrand() {
+    return (
+    <div className={styles.body1}>
+
+<Image src='https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104319/photo-townhouse-model-b-1_o4p0av.webp' width={500} height={500} className={styles.heroImage} alt=''/>
+
+<section className={styles.section}>
+<h1>CASA MIRA SOUTH NAGA CITY, CEBU</h1>
+<p>Awarded as 2018&rsquo;s Best Housing Development (Cebu), this beautifully designed economic community sits on a rolling terrain 60 to 100 meters above sea level that boasts expansive views and 45% open spaces.</p>
+<p>It features a grand entrance that is right beside the national highway and generously offers more with its three amenity areas all for the community to enjoy. At Casa Mira South there&rsquo;s MORE for the family.</p>
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104311/photo-clubhouse-retail_gi7zj8.webp"  alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px"/>
+</section>
+<hr />
+
+<section className={styles.section}>
+<h2>ACCESSBILITY &amp; LANDMARKS:</h2>
+<p>Just 14 kms away or 30 minutes via car from the South Road Properties.</p>
+<p>&spades; Along the National Road (Accessible thru PUJs &amp; PUVs)</p>
+<p>&spades; Few minutes going to:</p>
+<ul>
+<li>Mall</li>
+<li>Groceries</li>
+<li>Market</li>
+<li>Schools</li>
+<li>Restaurants</li>
+<li>Pharmacies</li>
+<li>Hospitals</li>
+</ul>
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739106414/464307193_8579523115462248_2292966776145292370_n_icbtzx.jpg"  alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px" />
+</section>
+<hr />
+
+<section className={styles.section}>
+<h2>AMENITIES &amp; FACILITIES:</h2>
+<ul>
+<li>Grand Entrance</li>
+<li>2 Entrances with Guardhouse</li>
+<li>Main Amenity Area</li>
+<li>Drop-off Pavilion</li>
+<li>Grand Community Clubhouse</li>
+<li>Retail Area</li>
+<li>Function Room</li>
+<li>Adult &amp; Kiddie Pool</li>
+<li>Multi-purpose Court</li>
+<li>Two Secondary Amenity Areas</li>
+<li>Pavilion</li>
+<li>Multi-purpose Court</li>
+<li>Retail Area</li>
+<li>Chapel with Function Hall</li>
+<li>Landscape Parks &amp; Open Spaces</li>
+<li>Street Lighting</li>
+<li>Perimeter Fence</li>
+<li>Overhead Water Tank</li>
+<li>Sewage Treatment Facility</li>
+<li>Material Recovery Facility</li>
+<li>20 meter wide main road</li>
+<li>24 Hour Security</li>
+<li>Property Management Services</li>
+</ul>
+<div className={styles.flexImage}>
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104312/1-1_avrl1o.webp"  alt=""  />
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104312/2-1_uinxsf.webp"  alt=""  />
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104311/5_wnppw0.webp"  alt=""  />
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739105443/6_l81d0q.webp"  alt=""  />
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104312/2_rtp77l.webp"  alt=""  />
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104311/photo-clubhouse-retail_gi7zj8.webp"  alt="" />
+</div>
+</section>
+<hr />
+
+<section className={styles.section}>
+<h3><strong>MODEL UNITS:</strong></h3>
+<ol>
+<li>TOWNHOUSE A&amp;B</li>
+<li>TOWNHOUSE C</li>
+</ol>
+<div className={styles.flexImage}>
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104311/3_uehfbw.webp"  alt=""  />
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104312/photo-townhouse-model-b-1-1_vf6091.webp"  alt="" />
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104311/photo-townhouse-model-c-1-1_tzhp27.webp"  alt=""  />
+</div>
+</section>
+<hr />
+
+<section className={styles.section}>
+<h2>UNIT DETAILS &amp; COMPUTATION:</h2>
+<h3>TOWNHOUSE A</h3>
+<p>Unit Details:</p>
+<ul>
+<li>2 Bedrooms</li>
+<li>1 Toilet &amp; Bath</li>
+<li>Balconette</li>
+<li>Carport</li>
+<li>Service Area</li>
+</ul>
+<p><strong>Ground Floor:</strong></p>
+<ul>
+<li>Carport</li>
+<li>Living Area</li>
+<li>Dining Area</li>
+<li>Kitchen Area</li>
+<li>Service Area</li>
+</ul>
+<p><strong>Second Floor:</strong></p>
+<ul>
+<li>Bedroom 1</li>
+<li>Bedroom</li>
+<li>Common Toilet &amp; Bath</li>
+<li>Hallway</li>
+</ul>
+<p>LOT AREA: 42 sq.m.</p>
+<p>FLOOR AREA: 36.2 sq.m.</p>
+<h3>SAMPLE COMPUTATION THROUGH BANK/PAG-IBIG:</h3>
+<p>TOTAL CONTRACT PRICE:&nbsp;<strong>Php 1,702,228.47</strong></p>
+<p>RESERVATION FEE:&nbsp;<strong>Php 10,000.00</strong></p>
+<p>&spades; 12% Downpayment spread over&nbsp;<strong>30</strong>&nbsp;months:</p>
+<ul>
+<li><strong>Php 5,900.00/month</strong></li>
+</ul>
+<p>&spades; 88% Balance paid in CASH/BANK FINANCING:&nbsp;<strong>Php 1,354,724.98</strong></p>
+<ul>
+<li><strong>20 years: Php 14,447/month</strong></li>
+</ul>
+<p>or</p>
+<p>&spades; 88% through PAG-IBIG<em>&nbsp;(subject for approval and assessment)</em>:<strong>&nbsp;Php 1,354,724.98</strong></p>
+<ul>
+<li>30 years:&nbsp;<strong>Php 8,341.27/month</strong></li>
+</ul>
+<div className={styles.flexImage}>
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104312/photo-townhouse-model-b-1-1_vf6091.webp"  alt="" />
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104320/townhouse-a-1_hiwpps.webp"  alt=""  />
+</div>
+</section>
+<hr />
+
+<section className={styles.section}>
+<h2>TOWNHOUSE A END:</h2>
+<p>Lot Area: 58.00 sq.m.</p>
+<p>Floor Area: 36.2 sq.m.</p>
+<p><strong>Ground Floor:</strong></p>
+<ul>
+<li>Carport</li>
+<li>Living Area</li>
+<li>Dining Area</li>
+<li>Kitchen Area</li>
+<li>Service Area</li>
+</ul>
+<p><strong>Second Floor:</strong></p>
+<ul>
+<li>Bedroom 1</li>
+<li>Bedroom</li>
+<li>Common Toilet &amp; Bath</li>
+<li>Hallway</li>
+</ul>
+<h4>SAMPLE COMPUTATION THROUGH BANK/PAGIBIG:</h4>
+<p>TOTAL CONTRACT PRICE:&nbsp;<strong>Php 1,971,438.10</strong></p>
+<p>RESERVATION FEE:&nbsp;<strong>Php 10,000.00</strong></p>
+<p>&diams; 12% Downpayment spread over&nbsp;<strong>30</strong>&nbsp;months:</p>
+<ul>
+<li><strong>Php 6,900.00/month</strong></li>
+</ul>
+<p>&diams; 88% Balance paid in CASH/BANK FINANCING:&nbsp;<strong>Php 1,578,696.32</strong></p>
+<ul>
+<li><strong>20 years: Php 16,836/month</strong></li>
+</ul>
+<p>or</p>
+<p>&diams; 88% through PAG-IBIG<em>&nbsp;(subject for approval and assessment)</em>:&nbsp;<strong>Php 1,578,696.32</strong></p>
+<ul>
+<li>30 years:&nbsp;<strong>Php9,720.30/month</strong></li>
+</ul>
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104320/1-2-scaled_woebof.webp"  alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px"/>
+</section>
+<hr />
+
+<section className={styles.section}>
+<h3>TOWNHOUSE B</h3>
+<p>Unit Details:</p>
+<ul>
+<li>2 Bedrooms</li>
+<li>1 Toilet &amp; Bath</li>
+<li>1 Powder Room</li>
+<li>Balconette</li>
+<li>Carport</li>
+<li>Service Area</li>
+</ul>
+<p><strong>Ground Floor:</strong></p>
+<ul>
+<li>Carport</li>
+<li>Living Area</li>
+<li>Dining Area</li>
+<li>Kitchen Area</li>
+<li>Service Area</li>
+<li>Powder Room</li>
+</ul>
+<p><strong>Second Floor:</strong></p>
+<ul>
+<li>Bedroom 1</li>
+<li>Bedroom 2</li>
+<li>Common Toilet &amp; Bath</li>
+<li>Hallway</li>
+</ul>
+<h4>SAMPLE COMPUTATION THROUGH BANK/PAG-IBIG</h4>
+<p>Lot Area: 48.00 sq.m.</p>
+<p>Floor Area: 46.25 sq.m.</p>
+<p>TOTAL CONTRACT PRICE:&nbsp;<strong>Php 2,091,336.33</strong></p>
+<p>RESERVATION FEE:&nbsp;<strong>Php 10,000.00</strong></p>
+<p>&diams;&nbsp;<strong>12%</strong>&nbsp;Downpayment spread over 30 months:</p>
+<ul>
+<li><strong>Php 7,400.00/month</strong></li>
+</ul>
+<p>&diams;&nbsp;<strong>88%</strong>&nbsp;Balance paid in CASH/BANK FINANCING:&nbsp;<strong>Php 1,676,807.86</strong></p>
+<ul>
+<li><strong>20 years: Php 17,882/month</strong></li>
+</ul>
+<p>or</p>
+<p>&diams; 88% through PAG-IBIG<em>&nbsp;(subject for approval and assessment)</em>:&nbsp;<strong>Php 1,676,807.86</strong></p>
+<ul>
+<li>30 years:&nbsp;<strong>Php 10,324.39/month</strong></li>
+</ul>
+<div className={styles.flexImage}>
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104312/photo-townhouse-model-b-1-1_vf6091.webp"   alt=""  />
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104312/photo-townhouse-model-b-1-1_vf6091.webp"  alt="" />
+</div>
+</section>
+
+<hr />
+<section className={styles.section}>
+<h3>TOWNHOUSE B END UNIT:</h3>
+<p>Unit Details:</p>
+<ul>
+<li>2 Bedrooms</li>
+<li>1 Toilet &amp; Bath</li>
+<li>1 Powder Room</li>
+<li>Balconette</li>
+<li>Carport</li>
+<li>Service Area</li>
+</ul>
+<p><strong>Ground Floor:</strong></p>
+<ul>
+<li>Carport</li>
+<li>Living Area</li>
+<li>Dining Area</li>
+<li>Kitchen Area</li>
+<li>Service Area</li>
+<li>Powder Room</li>
+</ul>
+<p><strong>Second Floor:</strong></p>
+<ul>
+<li>Bedroom 1</li>
+<li>Bedroom 2</li>
+<li>Common Toilet &amp; Bath</li>
+<li>Hallway</li>
+</ul>
+<h4>SAMPLE COMPUTATION THROUGH BANK/PAG-IBIG</h4>
+<p>Lot Area: 61.00 sq.m.</p>
+<p>Floor Area: 46.25 sq.m.</p>
+<p>TOTAL CONTRACT PRICE:&nbsp;<strong>Php 2,692,331.28</strong></p>
+<p>RESERVATION FEE:<em>&nbsp;Php 10,000.00</em></p>
+<p>&diams; 12% Downpayment spread over 30 months:</p>
+<ul>
+<li><strong>Php 9,400.0/month</strong></li>
+</ul>
+<p>&diams; 88% Balance paid in CASH/BANK FINANCING:&nbsp;<strong>Php 2,137,936.37</strong></p>
+<ul>
+<li><strong>20 years: Php 22,800/month</strong></li>
+</ul>
+<p>or</p>
+<p>&diams; 88% through PAG-IBIG<em>&nbsp;(subject for approval and assessment)</em>:&nbsp;<strong>Php 2,137,936.37</strong></p>
+<ul>
+<li>30 years:&nbsp;<strong>Php 13,163.64/month</strong></li>
+</ul>
+<FullImage  src="https://res.cloudinary.com/dnh0z6fm7/image/upload/v1739104321/CMS-UNIT-B-END-UNITweb-scaled_elfmsp.webp"  alt="" className={styles.fullImage} sizes="(max-width: 1540px) 100vw, 1540px" />
+
+</section>
+
+
+    </div>
+)
+}
