640e213d42818c63670f08bec5723dbd93255378
Jes2ni16 <jhontobit16@gmail.com>
2025-03-01 01:38:03 +0800

    add profile

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,18 +3,6 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://zaiko.website/client/malvin2/featured/lemenda-busay',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.9,
-    },
-    {
-      url: 'https://zaiko.website/client/malvin2/featured/prime-lapu',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.9,
-    },
-    {
       url: 'https://zaiko.website/client/malvin2/featured/primeworld-pointe',
       lastModified: new Date(),
       changeFrequency: 'monthly',
