c4ec97ff0a33b11818411307f5c7ce6a4187b01f
Jes2ni16 <jhontobit16@gmail.com>
2025-03-01 01:51:49 +0800

    add profile

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,9 +4,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://zaiko.website/client/malvin2/featured/primeworld-pointe',
-      lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
-    },
+    }
   ]
 }
